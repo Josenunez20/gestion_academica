a3899843ad42c1c7da2651785c950d8aebaae218
Jose Nuñez <joseurpino@gmail.com>
2026-04-15 15:20:38 -0400

    Conexion a la base de datos postgres

--- gestion_academica/settings.py
+++ gestion_academica/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'gestion_academica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestion_academica',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
